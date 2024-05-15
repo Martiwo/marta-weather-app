558e9fa69fcc3e02a14bab7a5fb0e9d78d5963f5
Martiwo <wolskamarta0@gmail.com>
2024-05-15 18:58:46 +0100

    add basic search engine behaviour (no API)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-alert("Hi! 🌟");
+function handelSearchSubmit(event) {
+  event.preventDefault();
+  let searchImput = document.querySelector("#search-form-imput");
+  console.log(searchImput.value);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchImput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handelSearchSubmit);
