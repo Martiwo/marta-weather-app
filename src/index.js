a2931b8ef1b5d1aee67a748c6f40c7e1763ad281
Martiwo <wolskamarta0@gmail.com>
2024-05-15 19:38:43 +0100

    added axios  and API key plus API URL created

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
+function updateWeather(response) {
+  console.log(response.data);
+}
+
+function searchCity(city) {
+  let apiKey = "8d0f974875ac7d348e4bt2300e1o239e";
+  let apiURL = `"https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric"`;
+  axios.get(apiURL).then(updateWeather);
+}
+
 function handelSearchSubmit(event) {
   event.preventDefault();
   let searchImput = document.querySelector("#search-form-imput");
   console.log(searchImput.value);
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchImput.value;
+  searchCity(searchImput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
